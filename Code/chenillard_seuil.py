635c3760c894eb92e48b40f8e1c538b71f8959da
Safenein <safenein@protonmail.com>
2022-11-16 18:31:33 +0000

    ajout du code pour le chenillard et le seuil

---
+++ Code/chenillard_seuil.py
@@ -0,0 +1,97 @@
+import time
+
+import gpio
+import smbus
+
+
+def read_i2c_pot(bus, address, CONFIG):
+    bus.write_byte(address, CONFIG)
+    time.sleep(0.1)
+    data = bus.read_i2c_block_data(address, 0x00, 2)
+    return data[0] * 256 + data[1]
+
+
+def get_waiting_time(pot):
+    if pot < 0.05:
+        pot = 0.05
+    return pot % 3
+
+
+def mode_chenille(bus, address, CONFIG, lp, bp):
+    state = [1, 0, 0, 0]  # Si besoin on peut changer l'affichage
+    offset = 0
+    fwd = True
+    while True:
+        while gpio.readpin(bp) is True:  # Tant que le bouton est relaché
+            offset = (offset + (1 if fwd else -1)) % len(state)
+            for i in range(len(lp)):
+                gpio.setpin(lp[i], state[(i + offset) % len(state)])
+            time.sleep(get_waiting_time(read_i2c_pot(bus, address, CONFIG)))
+        fwd = not fwd  # On inverse le sens
+        while gpio.readpin(bp) is False:  # Tant que le bouton est enfoncé
+            offset = (offset + (1 if fwd else -1)) % len(state)
+            for i in range(len(lp)):
+                gpio.setpin(lp[i], state[(i + offset) % len(state)])
+            time.sleep(get_waiting_time(read_i2c_pot(bus, address, CONFIG)))
+        # Donc le deuxième while sert d'anti rebond
+
+
+def light_threshold(pot, seuil, lp):
+
+    if pot > seuil[3]:  # Tout est allumé
+        for i in range(4):
+            gpio.setpin(lp[i], 0)
+
+    elif pot > seuil[2]:  # Tout sauf la dernière
+        for i in range(3):
+            gpio.setpin(lp[i], 0)
+        gpio.setpin(lp[3], 1)
+
+    elif pot > seuil[1]:  # Les deux premières led sont allumées
+        for i in range(2):
+            gpio.setpin(lp[i], 0)
+        for i in range(2):
+            gpio.setpin(lp[4 - i], 1)
+
+    elif pot > seuil[0]:  # La première est allumée
+        gpio.setpin(lp[0], 0)
+        for i in range(3):
+            gpio.setpin(lp[4 - i], 1)
+
+    else:  # Aucunes
+        for i in range(4):
+            gpio.setpin(lp[4 - i], 1)
+
+    return
+
+
+def mode_light_threshold(bus, address, CONFIG, lp):
+    seuil = [6553, 13107, 26214, 39321]
+    while True:
+        light_threshold(read_i2c_pot(bus, address, CONFIG), seuil, lp)
+        time.sleep(0.1)
+
+
+def main():
+    bus = smbus.SMBus(1)
+    address = 0x68
+    CONFIG = 0x10 | 0x00 | 0x00 | 0x40
+
+    lp = [7, 8, 25, 24]
+    bp = 12
+
+    gpio.closepin(bp)
+    gpio.initpin(bp, "in")
+
+    for i in range(len(lp)):
+        gpio.closepin(lp[i])
+        gpio.initpin(lp[i], "out")
+
+    if input("Mode seuil (1) ou mode chenille (2) : ") == "1":
+        mode_light_threshold(bus, address, CONFIG, lp)
+    else:
+        mode_chenille(bus, address, CONFIG, lp, bp)
+
+
+if __name__ == "__main__":
+    main()
